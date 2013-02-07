96daebc2f4bef3280de0a1640014cd6160ffaeba
Bartosz Krzeszewski <bartek@core-t.pl>
2013-02-07 20:04:37 +0100

    m

---
+++ application/views/helpers/GoogleAnalytics.php
@@ -0,0 +1,22 @@
+<?php
+
+class Zend_View_Helper_Websocket extends Zend_View_Helper_Abstract {
+
+    public function Websocket() {
+
+        $this->view->headScript()->appendFile($this->view->baseUrl() . '/js/swfobject.js');
+        $this->view->headScript()->appendFile($this->view->baseUrl() . '/js/web_socket.js');
+
+        $url = Zend_Registry::get('config')->websockets->aSchema . '://' . Zend_Registry::get('config')->websockets->aHost . ':' . Zend_Registry::get('config')->websockets->aPort . Zend_Registry::get('config')->websockets->aContext;
+
+        $script = '
+        WEB_SOCKET_SWF_LOCATION = "WebSocketMain.swf";
+        WEB_SOCKET_DEBUG = true;
+
+        var wsURL = "' . $url . '";
+';
+        $this->view->headScript()->appendScript($script);
+    }
+
+}
+
