0fef1765e6528e70439e643fd78bb4c1e358f1f5
Bartosz Krzeszewski <bartek@core-t.pl>
2013-07-23 02:23:47 +0200

    m

---
+++ public/js/game/timer.js
@@ -0,0 +1,23 @@
+function chat(color, msg, time) {
+    if (color != my.color) {
+        titleBlink('Incoming chat!');
+    }
+
+    var chatWindow = $('#chatWindow div').append('<br/>').append('<span style="color:' + color + '">' + color + ' (' + time + '): </span>' + msg);
+    var scroll = 120 - chatWindow[0].scrollHeight;
+    chatWindow.animate({
+        'top': scroll
+    }, 100);
+    $('#msg').focus();
+}
+
+function renderChatHistory() {
+    var chatWindow;
+    var scroll;
+    for (i in chatHistory) {
+        var chatWindow = $('#chatWindow div').append('<br/>').append('<span style="color:' + chatHistory[i]['color'] + '">' + chatHistory[i]['color'] + ' (' + getISODateTime(chatHistory[i]['date']) + '): </span>' + chatHistory[i]['message']);
+        var scroll = 120 - chatWindow[0].scrollHeight;
+        chatWindow.css({'top': scroll});
+    }
+
+}
