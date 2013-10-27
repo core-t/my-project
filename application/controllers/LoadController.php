2dce25c6f9d1b2f2b8e26cd0e7447649836b584b
Bartosz Krzeszewski <bartek@core-t.pl>
2013-10-27 23:21:58 +0100

    m

--- application/controllers/LoadController.php
+++ application/controllers/LoadController.php
@@ -10,23 +10,5 @@ class LoadController extends Game_Controller_Gui
         $this->view->myGames = $modelGame->getMyGames($this->_namespace->player['playerId'], $this->_request->getParam('page'));
     }
 
-    public function loadAction()
-    {
-        $this->_helper->layout->disableLayout();
-        $this->_helper->viewRenderer->setNoRender(true);
-
-        $gameId = $this->_request->getParam('gameId');
-        if (empty($gameId)) {
-            throw new Exception('Brak gameId!');
-        }
-//        $modelGame = new Application_Model_Game($gameId);
-//        if ($modelGame->playerIsAlive($this->_namespace->player['playerId'])) {
-            $this->_namespace->gameId = $gameId; // zapisuję gemeId do sesji
-            $this->_redirect('/' . Zend_Registry::get('lang') . '/game/index/id/' . $gameId);
-//        } else {
-//            throw new Exception('Nie powinno Cię tu być!');
-//        }
-    }
-
 }
 
