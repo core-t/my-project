39720e16086160a607938cacaa7dae2039be5b13
Bartosz Krzeszewski <bartek@core-t.pl>
2013-11-19 17:58:03 +0100

    m

--- scripts/mp32ogg.sh
+++ scripts/mp32ogg.sh
@@ -6,3 +6,11 @@ do
 		ffmpeg -i $I -codec:a libvorbis -q:a 3 $ogg
 	fi
 done
+
+for I in ../public/sound/background/*.mp3
+do
+        ogg=$(echo $I | sed -e s/mp3/ogg/)
+        if [ ! -f $ogg ]; then
+                ffmpeg -i $I -codec:a libvorbis -q:a 3 $ogg
+        fi
+done
